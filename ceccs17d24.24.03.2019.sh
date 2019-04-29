2dde6a1fdf9b1caa999726b1e1cca0f6c73a6655
Greshma Shaji <43352861+ceccs17d24@users.noreply.github.com>
2019-04-29 22:17:45 +0530

    (t2)shell_script

--- ceccs17d24.24.03.2019.sh
+++ ceccs17d24.24.03.2019.sh
@@ -1,26 +1,26 @@
 Greshma Shaji
 Roll no: 24
 
-pdftotext -nopgbrk result1.pdf
-grep --no-group-separator -A3 'CHN17CS' result1.txt | tr '\n' ' ' | sed 's/\ CHN/\nCHN/g' > result.txt
-sed -i 's/(O)/ 10  /g' result.txt
-sed -i 's/(A+)/ 9  /g' result.txt
-sed -i 's/(A)/ 8.5  /g' result.txt
-sed -i 's/(B+)/ 8  /g' result.txt
-sed -i 's/(B)/ 7  /g' result.txt
-sed -i 's/(C)/  6  /g' result.txt
-sed -i 's/(P)/  5  /g' result.txt
-sed -i 's/(F)/  0  /g' result.txt
-sed -i 's/(FE)/  0  /g' result.txt
-sed -i 's/(I)/ 0 /g' result.txt
-awk '{s=$3*4+$6*4+$9*4+$12*3+$15*3+$18*3+$21*1+$24*1+$27*1}{r=s/24}{print r}' result.txt >sgpa.txt
-awk '{print $1}' result.txt >reg1.txt
-paste -d ' \t ' reg1.txt sgpa.txt >res.txt
+pdftotext -nopgbrk result_CHN.pdf
+grep --no-group-separator -A3 'CHN17CS' result_CHN.txt | tr '\n' ' ' | sed 's/\ CHN/\nCHN/g' > result_cs.txt
+sed -i 's/(O)/ 10  /g' result_cs.txt
+sed -i 's/(A+)/ 9  /g' result_cs.txt
+sed -i 's/(A)/ 8.5  /g' result_cs.txt
+sed -i 's/(B+)/ 8  /g' result_cs.txt
+sed -i 's/(B)/ 7  /g' result_cs.txt
+sed -i 's/(C)/  6  /g' result_cs.txt
+sed -i 's/(P)/  5  /g' result_cs.txt
+sed -i 's/(F)/  0  /g' result_cs.txt
+sed -i 's/(FE)/  0  /g' result_cs.txt
+sed -i 's/(I)/ 0 /g' result_cs.txt
+awk '{s=$3*4+$6*4+$9*4+$12*3+$15*3+$18*3+$21*1+$24*1+$27*1}{r=s/24}{print r}' result_cs.txt >s1_sgpa.txt
+awk '{print $1}' result_cs.txt >reg.txt
+paste -d ' \t ' reg.txt s1_sgpa.txt >res.txt
 awk 'NR==FNR {seen[$3]=1; next} seen[$1]==1 {seen[$2]++; print $2}' s4d.txt res.txt >s1sgpa.txt
-awk '{print $3}' s4d.txt >reg1.txt
-paste -d ' \t ' reg1.txt s1sgpa.txt >s1final.txt
-pdftotext -nopgbrk result2.pdf
-grep --no-group-separator -A3 'CHN17CS' result2.txt | tr '\n' ' ' | sed 's/\ CHN/\nCHN/g' > results2.txt
+awk '{print $3}' s4d.txt >reg.txt
+paste -d ' \t ' reg.txt s1sgpa.txt >s1final.txt
+pdftotext -nopgbrk s2results.pdf
+grep --no-group-separator -A3 'CHN17CS' s2results.txt | tr '\n' ' ' | sed 's/\ CHN/\nCHN/g' > results2.txt
 sed -i 's/(O)/ 10  /g' results2.txt
 sed -i 's/(A+)/  9  /g' results2.txt
 sed -i 's/(A)/  8.5  /g' results2.txt
@@ -35,14 +35,14 @@ awk '{s=$3*4+$6*3+$9*3+$12*1+$15*1+$18*4+$21*3+$24*3+$27*1}{r=s/23}{print r}' re
 awk '{print $1}' results2.txt >reg2.txt
 paste -d ' \t ' reg2.txt s2sgpa.txt >resu.txt
 awk 'NR==FNR {seen[$3]=1; next} seen[$1]==1 {seen[$2]++; print $2}' s4d.txt resu.txt >sgpas2.txt
-awk '{print $3}' s4d.txt >reg1.txt
-paste -d ' \t ' reg1.txt sgpas2.txt >s2final.txt
+awk '{print $3}' s4d.txt >reg.txt
+paste -d ' \t ' reg.txt sgpas2.txt >s2final.txt
 paste -d ' \t ' s1final.txt sgpas2.txt >s1_s2.txt
 awk '{s=$2+$3}{r=s/2}{print r}' s1_s2.txt >cgpa.txt
-paste -d ' \t ' reg1.txt cgpa.txt >final_cgpa.txt
+paste -d ' \t ' reg.txt cgpa.txt >final_cgpa.txt
 awk '{print $4}' s4d.txt >name.txt
 echo "Reg no.  Name  s1 sgpa  s2 sgpa  Final cgpa"
-paste -d ' \t  \t ' reg1.txt name.txt >A.txt
+paste -d ' \t  \t ' reg.txt name.txt >A.txt
 paste -d ' \t  \t ' A.txt s1sgpa.txt >B.txt
 paste -d ' \t  \t ' B.txt sgpas2.txt >C.txt
 paste -d ' \t  \t ' C.txt cgpa.txt >s4dfinaldoc.txt
